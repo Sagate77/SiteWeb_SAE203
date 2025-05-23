26df1a3f4ddc77cd4c206fa88190063bd1cd5eeb
Sagate77 <150669465+Sagate77@users.noreply.github.com>
2025-05-23 22:55:53 +0200

    Add files via upload

---
+++ bdd/utilisateurs.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 23 mai 2025 à 22:55
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `utilisateurs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `prenom` varchar(100) DEFAULT NULL,
+  `num_etudiant` varchar(50) DEFAULT NULL,
+  `adresse_email` varchar(100) DEFAULT NULL,
+  `groupe_tp` varchar(50) DEFAULT NULL,
+  `materiel` varchar(100) DEFAULT NULL,
+  `signature` text DEFAULT NULL,
+  `date_reservation` date DEFAULT NULL,
+  `heure_debut` time DEFAULT NULL,
+  `heure_fin` time DEFAULT NULL,
+  `statut` varchar(20) DEFAULT 'en_attente',
+  `commentaire` text DEFAULT NULL,
+  `signature_admin` text DEFAULT NULL,
+  `horodatage` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) DEFAULT NULL,
+  `prenom` varchar(100) DEFAULT NULL,
+  `pseudo` varchar(100) DEFAULT NULL,
+  `date_naissance` date DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `adresse` text DEFAULT NULL,
+  `role` varchar(50) DEFAULT NULL,
+  `mot_de_passe` varchar(255) DEFAULT NULL,
+  `valide` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `nom`, `prenom`, `pseudo`, `date_naissance`, `email`, `adresse`, `role`, `mot_de_passe`, `valide`) VALUES
+(14, 'ghgfhgfh', 'fghfgh', 'fghhggfh', '0000-00-00', 'kiliancorpet@gmail.com', 'hgfhfghfgh', 'Administrateur', '$2y$10$agLu4lWlqdTj6hbr9XFNsey9zBWvgzQxkCmsJ8oPWjcLUd.hXTq7G', 1),
+(20, 'sada', 'sdasd', 'asdas', '0000-00-00', 'asdasdet@gmail.com', 'asdasd', 'Etudiant', '$2y$10$RdrQR/Hoc4AjetLvmgjINeKB/Z/UdEyBZH16PfIbgOHXqja8Xpf5K', 0);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
